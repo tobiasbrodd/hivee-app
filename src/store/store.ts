ea9e7ec6df6e4d0e3847f6d1ea6a58003ebcfc38
Tobias Brodd <tobias.brodd@gmail.com>
2021-04-11 12:44:25 +0200

    added support for persistant states

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,15 +2,30 @@ import { configureStore } from '@reduxjs/toolkit';
 import titleReducer from './titleSlice';
 import themeReducer from './themeSlice';
 import menuReducer from './menuSlice';
+import { loadState, saveState } from './localStorage';
+
+const preloadedState = loadState();
 
 const store = configureStore({
     reducer: {
         title: titleReducer,
         theme: themeReducer,
         menu: menuReducer,
-    }
+    },
+    preloadedState
 });
 
+store.subscribe(() => {
+    saveState({
+        theme: store.getState().theme
+    })
+});
+
+export default store;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-export default store;
+export type StorageState = {
+    theme: {
+        value: string
+    }
+}
