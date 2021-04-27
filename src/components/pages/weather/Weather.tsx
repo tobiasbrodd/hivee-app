132bb2eb0e8b676b58630b7b8e716cc2688de3ca
Tobias Brodd <tobias.brodd@gmail.com>
2021-04-28 00:54:50 +0200

    added weather card

--- src/components/pages/weather/Weather.tsx
+++ src/components/pages/weather/Weather.tsx
@@ -3,6 +3,8 @@ import ScrollToTop from '../../common/scroll/Scroll';
 import { setTitle } from '../../../store/slices/title/titleSlice';
 import { useAppDispatch } from '../../../store/hooks';
 import { useEffect } from 'react';
+import WeatherCard from '../../common/cards/WeatherCard';
+import styled from 'styled-components';
 
 const title = "Weather";
 
@@ -23,7 +25,19 @@ export default function Weather() {
                 <title>{helmet.title}</title>
                 <meta name="description" content={helmet.description} />
             </Helmet>
+            <WeatherContainer>
+                <WeatherCard />
+            </WeatherContainer>
             <ScrollToTop />
         </div>
     );
 }
+
+const WeatherContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: left;
+    align-items: center;
+    width: 100%;
+`;
